76c72dec2a892651fc6766f6b3877af323f9a958
Tom Wilczak <tomwilczak@Toms-MacBook-Pro.local>
2020-03-10 10:28:50 -0500

    Create self-signed cert - add https process

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
+const fs = require('fs');
 const app = require('express')();
 const cors = require('cors');
+const http = require('http');
+const https = require('https');
 const bodyParser = require('body-parser');
+
+const privateKey  = fs.readFileSync('./sslcert/server.key', 'utf8');
+const certificate = fs.readFileSync('./sslcert/server.cert', 'utf8');
+
+const credentials = {key: privateKey, cert: certificate};
+
+const httpPort = 3000;
+const httpsPort = 3030;
+
 const routes = require('./routes');
 
 app.use(cors());
@@ -8,6 +20,10 @@ app.use(bodyParser.text());
 app.use(bodyParser.json());
 app.use('/', routes);
 
-app.listen(3000, () => {
-  console.log('listening for connections on port 3000');
+http.createServer(app).listen(httpPort, () => {
+  console.log(`listening for connections on port ${httpPort}`);
+});
+
+https.createServer(credentials, app).listen(httpsPort, () => {
+  console.log(`listening for connections on port ${httpsPort}`);
 });
